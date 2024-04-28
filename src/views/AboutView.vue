09384fd16c9a5972f21015ddc548243d3759e8f5
LYR <lyr04202022@163.com>
2024-04-28 11:56:13 +0800

    初始项目

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+.about {
+  border: 1px solid red;
+  h1 {
+    color: purple;
+  }
+}
+</style>
